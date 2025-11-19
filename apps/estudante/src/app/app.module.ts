0a67489f2cdbba3a40aac570c7644de0899655ef
ardooedu <eduardosalima3@gmail.com>
2025-11-18 22:11:53 -0300

    feat: adicionando configModule para validar variaveis de ambiente

--- apps/estudante/src/app/app.module.ts
+++ apps/estudante/src/app/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DrizzleAsyncProvider, drizzleProvider } from './drizzle/drizzle.provider';
 import { CreateEstudanteController } from './useCases/create-estudante.controller';
+import { envSchema } from '../env';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot({
+	validate: (env) => envSchema.parse(env),
+  })],
   controllers: [CreateEstudanteController],
   providers: [...drizzleProvider],
   exports: [DrizzleAsyncProvider],
--- apps/estudante/src/main.ts
+++ apps/estudante/src/main.ts
@@ -6,12 +6,14 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
-
+import { ConfigService } from '@nestjs/config';
+import { Env } from './env';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
+  const configService: ConfigService<Env, true> = app.get(ConfigService);
+  const port = configService.get('ESTUDANTE_PORT', { infer: true });
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
