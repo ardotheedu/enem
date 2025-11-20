1047c908948cc78f1fa10253d0e404668ed0fcc7
ardotheedu <eduardosalima3@gmail.com>
2025-11-20 15:45:23 -0300

    feat: adiciona module jwt

--- apps/estudante/src/app/app.module.ts
+++ apps/estudante/src/app/app.module.ts
@@ -3,12 +3,13 @@ import { ConfigModule } from '@nestjs/config';
 import { DrizzleAsyncProvider, drizzleProvider } from './drizzle/drizzle.provider';
 import { CreateEstudanteController } from './useCases/create-estudante.controller';
 import { envSchema } from './env';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
     validate: env => envSchema.parse(env),
     isGlobal: true,
-  })],
+  }), AuthModule],
   controllers: [CreateEstudanteController],
   providers: [...drizzleProvider],
   exports: [DrizzleAsyncProvider],
