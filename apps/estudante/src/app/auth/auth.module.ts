a7bd1abbae3ccea73a0d4d2002d085a16c2664c7
ardotheedu <eduardosalima3@gmail.com>
2025-11-21 17:10:14 -0300

    feat: adiciona provider no auth module

--- apps/estudante/src/app/auth/auth.module.ts
+++ apps/estudante/src/app/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { Env } from '../env';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
     imports: [
@@ -21,7 +22,10 @@ import { Env } from '../env';
                 }
             }
         })
-    ]
+    ],
+	providers: [
+		JwtStrategy
+	]
 })
 export class AuthModule {
 
